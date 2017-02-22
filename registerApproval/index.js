4b4fe2373d08db9c786ba00eb325c5db4e5d7464
edobry <eugene@dobry.me>
2017-02-21 20:01:12 -0500

    testing almost works

--- registerApproval/index.js
+++ registerApproval/index.js
@@ -15,6 +15,10 @@ const github = new GitHubApi({
     Promise: Promise,
     timeout: 5000
 });
+github.authenticate({
+    type: "token",
+    token: nconf.get("GITHUB:TOKEN")
+});
 
 const hipchat = new Hipchatter();
 
@@ -29,15 +33,16 @@ const validHookConfig = (pingEvent) =>
     pingEvent.hook.events.some(event =>
         event == pullRequestReviewEvent);
 
-var handleGithubWebhook = event => {
-    if(isPingEvent(event)) {
-        if(validHookConfig(event))
-            return Promise.resolve("Configuration valid!");
-        else
-            return Promise.reject(new Error(
-                `Webhook configuration is incorrect! Expecting event ${pullRequestReviewEvent}`));
-    }
 
+const handlePing = event => {
+    if(validHookConfig(event))
+        return Promise.resolve("Configuration valid!");
+    else
+        return Promise.reject(new Error(
+            `Webhook configuration is incorrect! Expecting event ${pullRequestReviewEvent}`));
+};
+
+const handleReview = event => {
     const issueKey = event.pull_request.head.ref;
     const reviewer = event.sender.login;
 
@@ -45,13 +50,100 @@ var handleGithubWebhook = event => {
     if(event.review.state != "approved")
         return Promise.resolve("Was not an approval, too bad.");
 
-    return findGithubUserName(reviewer).then(user =>
+    return handleApproval(event);
+};
+
+const handleApproval = event =>
+    findGithubUserName(event.sender.login).then(user =>
         findJiraUsername(user)
             .then(addApproverTo(event.pull_request.head.ref))
             .then(lookupJiraIssue)
-            .then(notifyHipchat(user, event))
+            .then(notifyHipchatofApproval(user, event))
             .then(() =>
                 console.log("Done! (Probably)")));
+
+
+const approvalMessage = (user, approval, issue) =>
+`${user.name} just approved <a href="${approval.review.html_url}">${approval.pull_request.title}</a>
+for issue <a href="${issueUrl(issue)}">[${issue.key}] - ${issue.fields.summary}</a>`;
+
+const notifyHipchatofApproval = (user, event) => issue =>
+    notifyHipchat(approvalMessage(user, event, issue));
+
+const testPassMessage = (user, event, issue) =>
+`${user.name} just successfully tested <a href="${event.comment.html_url}">${event.issue.title}</a>
+for issue <a href="${issueUrl(issue)}">[${issue.key}] - ${issue.fields.summary}</a>`;
+
+const notifyHipchatofTestPass = (user, event) => issue =>
+    notifyHipchat(testPassMessage(user, event, issue));
+
+const findIssueBranch = event =>
+    github.pullRequests.get({
+        owner: event.repository.owner.login,
+        repo: event.repository.name,
+        number: event.issue.number
+    }).then(pr => pr.head.ref);
+
+const handleTestResult = event => testPassed => {
+    if(!testPassed)
+        return Promise.resolve("Test was a fail, 2bad so sad.");
+
+    const issueBranchP = findIssueBranch(event.issue.number)
+        .catch(err => console.log(err));
+
+    return findGithubUserName(event.sender.login)
+        .then(findJiraUsername)
+        .then(user =>
+            issueBranchP
+                .then(addTester(user))
+                .then(lookupJiraIssue)
+                .then(notifyHipchatofTestPass(user, event))
+                .then(() =>
+                    console.log("Done! (Probably)")));
+};
+
+const testResultPattern = /\[Test: (Pass|Fail)\]/;
+const testResults = {
+    Pass: true,
+    Fail: false
+};
+const parseComment = comment => {
+    console.log(`Parsing comment ${comment}`);
+    const match = testResultPattern.exec(comment);
+    if(!match) {
+        console.log("Comment was not related to testing.");
+        return Promise.reject();
+    }
+
+    return Promise.resolve(testResults[match[1]]);
+};
+
+const handleComment = event => {
+    if(["created", "edited"].indexOf(event.action) < 0) {
+        console.log("Don't care about comment deletions.");
+        return Promise.resolve();
+    }
+
+    return parseComment(event.comment.body)
+        .then(handleTestResult(event));
+};
+
+const routes = {
+    review: handleReview,
+    comment: handleComment
+};
+var handleGithubWebhook = request => {
+    const event = JSON.parse(request.body);
+
+    const action = request.resource.split('/')[1];
+    const handler = routes[action];
+
+    if(isPingEvent(event))
+        return handlePing(event);
+
+    console.log(`Handling: ${action}`);
+
+    return handler(event);
 };
 
 const losers = nconf.get("LOSERS");
@@ -94,7 +186,6 @@ var addApproverCommand = name => ({
             add: { name }
         }]
     }});
-
 var addApproverTo = issueKey => user => {
     var username = user.name;
 
@@ -104,6 +195,28 @@ var addApproverTo = issueKey => user => {
         json: true,
         auth: secretSecretJiraCredsShh,
         body: addApproverCommand(username)
+    }).catch(err =>
+        Promise.reject(err.response.body.errorMessages)
+    ).then(() => issueKey);
+};
+
+var testedByField = "customfield_11201";
+var addTesterCommand = name => ({
+    update: {
+        [testedByField]: [{
+            set: { name }
+        }]
+    }});
+
+var addTester = user => issueKey => {
+    var username = user.name;
+
+    console.log(`Setting ${username} as the tester of ${issueKey} on JIRA...`);
+    return request.put({
+        url: `${jiraAPI}/issue/${issueKey}`,
+        json: true,
+        auth: secretSecretJiraCredsShh,
+        body: addTesterCommand(username)
     }).catch(err =>
         Promise.reject(err.response.body.errorMessages)
     ).then(() => issueKey);
@@ -124,14 +237,9 @@ const lookupJiraIssue = issueKey => {
 
 const hipchatRoom = "Development";
 
-const approvalMessage = (approver, approval, issue) =>
-    `${approver.name} just approved <a href="${approval.review.html_url}">${approval.pull_request.title}</a>
-for issue <a href="${issueUrl(issue)}">[${issue.key}] - ${issue.fields.summary}</a>`;
-
-const notifyHipchat = (approver, approval) => issue => {
+const notifyHipchat = message => {
     console.log("Notifying Hipchat...");
 
-    const message = approvalMessage(approver, approval, issue);
     return new Promise((resolve, reject) =>
         hipchat.notify(hipchatRoom, {
             message,
@@ -147,7 +255,10 @@ const notifyHipchat = (approver, approval) => issue => {
 };
 
 exports.handler = (event, context, callback) => {
+    console.log(JSON.stringify(context));
+    console.log(JSON.stringify(event));
     handleGithubWebhook(event)
-        .then(callback)
+        .then(result =>
+            callback(null, result))
         .catch(callback);
 };
