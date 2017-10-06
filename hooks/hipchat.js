ff8edb7e5dda86e597e9a2d1edf47e42b914e454
Frank Camilleri <fcamilleri22@gmail.com>
2017-10-06 17:23:56 -0400

    WIP

--- hooks/hipchat.js
+++ hooks/hipchat.js
@@ -1,3 +1,11 @@
-"use strict";
+const
+    Hook = require("../hook");
 
-console.log("SHIET BOYEEE");
+
+//In the future, assuming we're going to want to have this deal with
+//multiple possible handlers.
+const parseHipchatTrigger = (trigger) => {
+
+}
+
+module.exports = new Hook([], parseHipchatTrigger ,()=>"potato")
