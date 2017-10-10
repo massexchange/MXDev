c71ec8cb66da6589f082d106392aee5574465d9a
Frank Camilleri <fcamilleri22@gmail.com>
2017-10-10 13:50:08 -0400

    Better comments.

--- events/mxcontrol.js
+++ events/mxcontrol.js
@@ -2,8 +2,10 @@ const Event = require("../event");
 
 /*
     MXDev-Formatted Wrapper for MXControl Control Task.
-    See MXControl/README.md > ControlTask Syntax for more information.
+    See MXControl/README.md > ControlTask Syntax for the makeup of this.task
+    See MXControl/README.md for more information regarding acceptable arguments.
 */
+
 module.exports = class MXControlEvent extends Event {
     constructor(trigger, action, targetType, target, size) {
         super(trigger);
@@ -25,6 +27,5 @@ module.exports = class MXControlEvent extends Event {
 
                     ? this.task = Object.assign({database: target}, controlTask)
                     : console.log("Invalid Target Type.");
-
     }
 };
