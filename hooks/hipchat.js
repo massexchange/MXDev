2227fe400965d320d0b918cf53912f886a5020a6
Frank Camilleri <fcamilleri22@gmail.com>
2017-10-10 14:02:37 -0400

    Getting there on the hookshot...

--- hooks/hipchat.js
+++ hooks/hipchat.js
@@ -1,20 +1,41 @@
 const
     Hook = require("../hook");
-
+    MXControlEvent = require("../events/mxcontrol"),
 
 //In the future, assuming we're going to want to have this deal with
 //multiple possible handlers.
 const parseHipchatTrigger = (trigger) => {
     //if it's not a message, do something else. Ionno yet.
-    return {
+    const parsedTrigger = {
         event: trigger.event,
         message: trigger.item.message.message,
         from: {
             id: trigger.item.message.from.id,
             mentionName: trigger.item.message.from.mention_name,
-            name: trigger.item.
+            name: trigger.item.message.from.name
+        }
+    };
+    const events = [];
 
+    if (parsedTrigger.event == "room_message") { //scaffold for future stuffs
+        const commandArgs = parsedTrigger.message.split(" ");
+        if (commandArgs[0] == "/mxcontrol") {
+            events.push(new MXControlEvent(
+                trigger,
+                commandArgs[1], //action
+                commandArgs[2], //targetType
+                commandArgs[3], //target
+                commandArgs[4]  //size
+            ));
+        }
     }
-}
 
-module.exports = new Hook([], parseHipchatTrigger ,()=>"potato");
+    return events;
+};
+
+// const parseMessage
+
+module.exports = new Hook(
+    [],
+    parseHipchatTrigger,
+    ()=>"potato");
