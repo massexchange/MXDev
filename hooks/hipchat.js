bbd237277a9bb6b44139447d606deaf68c2f3dfa
Frank Camilleri <fcamilleri22@gmail.com>
2017-10-10 14:49:25 -0400

    WIP and sample stuff

--- hooks/hipchat.js
+++ hooks/hipchat.js
@@ -1,12 +1,33 @@
-const
-    Hook = require("../hook");
-    MXControlEvent = require("../events/mxcontrol"),
 
-//In the future, assuming we're going to want to have this deal with
-//multiple possible handlers.
+const Hook = require("../hook");
+
+const MXControlEvent = require("../events/mxcontrol");
+const MXControlHandler = require("../handlers/mxcontrol");
+
 const parseHipchatTrigger = (trigger) => {
-    //if it's not a message, do something else. Ionno yet.
-    const parsedTrigger = {
+
+    const parsedTrigger = simplifyHipchatTrigger;
+
+    const events = [];
+
+    if (parsedTrigger.event == "room_message") {
+        const commandArgs = parsedTrigger.message.split(" ");
+
+        if (commandArgs[0] == "/mxcontrol")
+            events.push(new MXControlEvent(
+                trigger,
+                commandArgs[1], //action
+                commandArgs[2], //targetType
+                commandArgs[3], //target
+                commandArgs[4]  //size
+            ));
+    }
+
+    return events;
+};
+
+const simplifyHipchatTrigger = (trigger) => {
+    return {
         event: trigger.event,
         message: trigger.item.message.message,
         from: {
@@ -15,27 +36,13 @@ const parseHipchatTrigger = (trigger) => {
             name: trigger.item.message.from.name
         }
     };
-    const events = [];
-
-    if (parsedTrigger.event == "room_message") { //scaffold for future stuffs
-        const commandArgs = parsedTrigger.message.split(" ");
-        if (commandArgs[0] == "/mxcontrol") {
-            events.push(new MXControlEvent(
-                trigger,
-                commandArgs[1], //action
-                commandArgs[2], //targetType
-                commandArgs[3], //target
-                commandArgs[4]  //size
-            ));
-        }
-    }
-
-    return events;
 };
 
-// const parseMessage
 
 module.exports = new Hook(
-    [],
+    [MXControlHandler],
     parseHipchatTrigger,
-    ()=>"potato");
+    (trigger)=> {
+        console.log("Recieved Hipchat Message:");
+        console.log(simplifyHipchatTrigger(trigger));
+    });
