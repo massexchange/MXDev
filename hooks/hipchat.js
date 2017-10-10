20153e7e0d89624a99c72453acd5728cb219b045
Frank Camilleri <fcamilleri22@gmail.com>
2017-10-10 12:23:50 -0400

    More WIP

--- hooks/hipchat.js
+++ hooks/hipchat.js
@@ -5,7 +5,16 @@ const
 //In the future, assuming we're going to want to have this deal with
 //multiple possible handlers.
 const parseHipchatTrigger = (trigger) => {
+    //if it's not a message, do something else. Ionno yet.
+    return {
+        event: trigger.event,
+        message: trigger.item.message.message,
+        from: {
+            id: trigger.item.message.from.id,
+            mentionName: trigger.item.message.from.mention_name,
+            name: trigger.item.
 
+    }
 }
 
-module.exports = new Hook([], parseHipchatTrigger ,()=>"potato")
+module.exports = new Hook([], parseHipchatTrigger ,()=>"potato");
