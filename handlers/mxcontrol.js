4c4b2d9856fcb7a705c4608197e2ab5e912c3e9f
Frank Camilleri <fcamilleri22@gmail.com>
2017-10-12 16:02:42 -0400

    WIP. Just need to babel n deploy.

--- handlers/mxcontrol.js
+++ handlers/mxcontrol.js
@@ -1,9 +1,36 @@
 const nconf = require("nconf");
 const MXControlEvent = require("../events/mxcontrol");
 const Hipchat = require("../api/hipchat");
+const MXControl = require("mxcontrol");
+const Promise = require("bluebird");
 
-const handleMXControlTask = (event) =>{
-    
+nconf.env("_");
+
+const hipchat = new Hipchat(nconf.get("HIPCHAT:ROOM:MXCONTROL:TOKEN"));
+
+const handleMXControlTask = (event) => {
+    const {task} = event;
+    console.log("Handling MXControl Task...");
+    const targetName = task.instance || task.environment || task.database;
+
+    hipchat.notify(
+        MXControl.buildLog(
+            targetName,
+            task.action,
+            task.size,
+            task.database
+        ),
+        {room: "MXControl"}
+    );
+
+    return MXControl.runTask(task).then(()=>{
+        hipchat.notify(
+            `MXControl job for ${targetName} Completed.`,
+            {room: "MXControl"}
+        );
+        return Promise.resolve();
+    });
+
 }
 
 module.exports = {
