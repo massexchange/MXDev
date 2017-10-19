ab250d3ede6ef3b33c9f3de166d05e6e6fcb1d29
Frank Camilleri <fcamilleri22@gmail.com>
2017-10-19 18:15:33 -0400

    Now why the fuck was this so goddamn HARD?

--- handlers/mxcontrol.js
+++ handlers/mxcontrol.js
@@ -14,21 +14,19 @@ const handleMXControlTask = (event) => {
     const task = event.task;
     const targetName = task.instance || task.environment || task.database;
 
+    const logline =
+        MXControl.buildLog(targetName, task.action, task.size, task.database);
 
-    msgMXControlRoom(
-        MXControl.buildLog(
-            targetName,
-            task.action,
-            task.size,
-            task.database
-        )
-    );
+    msgMXControlRoom(logline);
 
     return Promise.resolve(MXControl.runTask(task).then((res)=>{
 
         const action = task.action.toLowerCase();
-        if (action == "status" || action == "info")
-            msgMXControlRoom(res);
+        if (action == "status" || action == "info") {
+            console.log(res);
+            msgMXControlRoom(JSON.stringify(res));
+        }
+
 
         else
             msgMXControlRoom(
