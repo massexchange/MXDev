6f0af60a7de1f9fe38b975c786039b63466e5d14
Frank Camilleri <fcamilleri22@gmail.com>
2017-12-12 16:15:37 -0500

    Only statuses wait.

--- src/handlers/mxcontrol.js
+++ src/handlers/mxcontrol.js
@@ -10,40 +10,39 @@ const hipchat = new Hipchat(nconf.get("HIPCHAT:ROOM:MXCONTROL:TOKEN"));
 const msgMXControlRoom =
     (message) => hipchat.notify(message, {room: "MXControl"});
 
-const handleMXControlTask = (event) => {
+const handleMXControlTask = async (event) => {
+    const statusVerbs = ["status","info","scry","check"];
+
     const task = event.task;
     const targetName = task.instance || task.environment || task.database;
+    const action = task.action.toLowerCase();
 
     const logline =
         MXControl.buildLog(targetName, task.action, task.size, task.database);
 
     msgMXControlRoom(logline);
 
-    return Promise.resolve(MXControl.runTask(task).then((res)=>{
+    if (statusVerbs.includes(action)) {
+        const statusResponse = await MXControl.runTask(task);
+        console.log(statusResponse);
+        msgMXControlRoom(
+            JSON.stringify(statusResponse)
+                .replace(/\[|\]|\}/g, "")
+                .replace(/,/g, "\n")
+                .replace(/\{/g, "\n--> ")
+                .replace(/"/g, "")
+                .replace(/:/g, ": ")
+        );
+        return;
+    }
 
-        const action = task.action.toLowerCase();
-
-        if (["status","info"].includes(action)) {
-            console.log(res);
-            msgMXControlRoom(
-                JSON.stringify(res)
-                    .replace(/\[|\]|\}/g, "")
-                    .replace(/,/g, "\n")
-                    .replace(/\{/g, "\n--> ")
-                    .replace(/"/g, "")
-                    .replace(/:/g, ": ")
-            );
-        }
-
-        return Promise.resolve();
-
-    }).catch(err =>{
+    MXControl.runTask(task).catch(err =>{
         //task failed for some reason -- shoot errors
         console.log(err);
         //attempt to notify Hipchat
         msgMXControlRoom(JSON.stringify(err));
         msgMXControlRoom("Something went wrong with the operation. Ops has been notified.");
-    }));
+    });
 
 };
 
