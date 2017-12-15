c3a61ddceae30010b935b3e18678b624cdab8eb4
Frank Camilleri <fcamilleri22@gmail.com>
2017-12-15 16:33:17 -0500

    Brainstorming

--- src/events/mxcontrol.js
+++ src/events/mxcontrol.js
@@ -7,25 +7,45 @@ const Event = require("../event");
 */
 
 module.exports = class MXControlEvent extends Event {
-    constructor(trigger, action, targetType, target, size) {
+    //second argument is a deconstructed Minimist args object.
+    //"_" is minimist parlance for unflagged arguments
+    constructor(trigger, {_, debug, sudo, env, inst, db, size}) {
         super(trigger);
 
-        let controlTask = {
-            action: action
-        };
+        const args = _;
 
-        //Adapted from MXControl/CLI.js
-        if (size) controlTask = Object.assign({size: size}, controlTask);
+        if (sudo)
+            debug = true; //sudo means non-status operations allowed on app
 
-        this.task =
-        ["env", "environment"].includes(targetType)
-            ?  Object.assign({environment: target}, controlTask)
-            : ["inst", "instance"].includes(targetType)
 
-                ? Object.assign({instance: target}, controlTask)
-                : ["db", "database"].includes(targetType)
 
-                    ? Object.assign({database: target}, controlTask)
-                    : console.log("Invalid Target Type.");
-    }
+        //If Debug mode is on, full mxcontrol cli is exposed in hipchatNotes
+
+        //Otherwise, simplified cli is exposed
+        // -- operations don't require target type
+        // -- only whole-environment operations allowed (no resizes)
+        // -- outputs are simplified
+
+        if (debug) { //Adapted, but not copied, from mxcontrol/cli.js
+
+        }
+
+    //     let controlTask = {
+    //         action: action
+    //     };
+    //
+    //     //Adapted from MXControl/CLI.js
+    //     if (size) controlTask = Object.assign({size: size}, controlTask);
+    //
+    //     this.task =
+    //     ["env", "environment"].includes(targetType)
+    //         ?  Object.assign({environment: target}, controlTask)
+    //         : ["inst", "instance"].includes(targetType)
+    //
+    //             ? Object.assign({instance: target}, controlTask)
+    //             : ["db", "database"].includes(targetType)
+    //
+    //                 ? Object.assign({database: target}, controlTask)
+    //                 : console.log("Invalid Target Type.");
+    // }
 };
