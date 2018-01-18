b09a63943b18e919319f72a845845040ac3dc635
Frank Camilleri <fcamilleri22@gmail.com>
2018-01-18 16:18:45 -0500

    Doublederp

--- src/handlers/mxcontrol.js
+++ src/handlers/mxcontrol.js
@@ -55,8 +55,8 @@ const handleMXControlEvent = async event => {
             (upVerbs.includes(action)
             || rebootVerbs.includes(action))
     ) {
-        await mxDynamoDB.putItem({"Name": `MXWeb-${targetName}`}, "mxDevTest");
-        await mxDynamoDB.putItem({"Name": `MXBackend-${targetName}`}, "mxDevTest");
+        await mxDynamoDB.putItem({"Name": {"S":`MXWeb-${targetName}`}}, "mxDevTest");
+        await mxDynamoDB.putItem({"Name": {"S":`MXBackend-${targetName}`}}, "mxDevTest");
         //no return -- let the rest of the task run...
     }
 
